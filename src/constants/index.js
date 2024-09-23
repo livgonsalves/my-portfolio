c8ee3f890be6c0805d35f3aa4e7e94b286fa6bc2
livgonsalves <livgonsalves1512@gmail.com>
2024-09-23 14:42:30 -0400

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,297 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:
+      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Mark Rogers',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:
+      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'John Dohsas',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Ether Smith',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Location-Based Reminder using Geofence Application',
+    desc: 'Developed a mobile application using boundary and black-box testing methodologies to ensure accurate geofence triggering. Utilized Google Maps API for location tracking. ',
+    subdesc:
+      'Tech: HTML, CSS, Node.js, Kotlin, Jenkins',
+    href: 'https://github.com/livgonsalves/Location_based_reminder_using_geofence',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Job Application Autofill Extension',
+    desc: 'Developed a Swift-based extension with a popup to save and autofill job application forms using stored data.',
+    subdesc:
+      'Tech: Swif, WebKit, UserDefaults, JavaScript',
+    href: 'https://github.com/livgonsalves/iJobHelper',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Dutch Auction Decentralized Application',
+    desc: 'Designed and implemented a decentralized auction platform for NFTs and ERC20 tokens, allowing seamless bidding and trading.',
+    subdesc:
+      'Tech: Solidity, Hardhat, ReactJS, IPFS ',
+    href: 'https://github.com/livgonsalves/DAMG6210-Group11-Project',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Exploring Sentimental Landscaping of Songs using NLP',
+    desc: 'Developed Bayesian and Semantic models on a comprehensive dataset, achieving an impressive 87% accuracy rate through 30 hours of model training.',
+    subdesc:
+      'Tech: Natural Language Processing (NLP), Bayesian models, Semantic models, Python, NLTK, PyTorch, TensorFlow ',
+    href: 'https://github.com/livgonsalves/Exploring_Sentimental_Landscaping_of_Songs_using_NLP',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Mental State Detection and Post-Treatment Application',
+    desc: 'Created a cross-platform application using Dart and Flutter, following the MVC architectural pattern, and implemented NLP algorithms for sentiment analysis and emotion recognition. ',
+    subdesc:
+      'Tech: Mobile Development, Machine Learning, Dart, Flutter, JavaScript. ',
+    href: 'https://github.com/livgonsalves/Mental_State_Detection',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Northeastern University',
+    pos: 'Graduate Teaching Assistant',
+    duration: 'September 2024 - Present',
+    title: "Served as a Teaching Assistant for ENCP 6000 Career Management for Engineers, supporting students in career development and management strategies.",
+    icon: '/assets/neu.jpeg',
+    animation: 'salute',
+  },
+  {
+    id: 2,
+    name: 'Dassault Systèmes',
+    pos: 'Software Engineer Intern',
+    duration: 'January 2024 - June 2024',
+    title: "Developed and enhanced ENOVIA's sourcing applications on the 3DEXPERIENCE platform using UWA, RequireJS, and advanced JavaScript techniques, integrating RESTful APIs, leading UI/UX improvements, and implementing CI/CD pipelines and Test-Driven Development for efficient, customer-focused solutions.",
+    icon: '/assets/ds.png',
+    animation: 'victory',
+  },
+  {
+    id: 3,
+    name: 'Northeastern University',
+    pos: 'Program Assistant',
+    duration: 'April 2023 - December 2023',
+    title: "Managed Linux environments, optimized testing procedures, collaborated on web-based software maintenance, and supported cloud migration projects on AWS and Azure to improve resource usage.",
+    icon: '/assets/neu.jpeg',
+    animation: 'clapping',
+  },
+  {
+    id: 4,
+    name: 'SangHarsh',
+    pos: 'Software Development Intern',
+    duration: 'January 2019 - May 2019',
+    title: "Developed a crowdfunding website using CSS3, HTML5, and JavaScript, raising $70,000 and enhancing donor engagement with features like a donation tracker and donor wall.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
