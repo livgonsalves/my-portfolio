2f45aa7016dd9a67d6f7f281773ce76304b82618
Livia Gonsalves <41815386+livgonsalves@users.noreply.github.com>
2024-09-24 11:27:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
+  base: 'my-portfolio', // Replace 'repo-name' with the actual name of your GitHub repository
   plugins: [react()],
-})
+};
