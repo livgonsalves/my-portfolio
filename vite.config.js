4affbc4a7ee4871cc70fa5799d06363d21b6fd42
livgonsalves <livgonsalves1512@gmail.com>
2024-09-27 14:33:43 -0400

    Build site and update for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: 'my-portfolio', // Replace 'repo-name' with the actual name of your GitHub repository
+export default defineConfig({
+  //base: '/',
   plugins: [react()],
-};
+})
